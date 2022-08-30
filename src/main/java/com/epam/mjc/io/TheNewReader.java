96991e0f5f54b3e607bfe9150daab86957c7c18a
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-08-30 16:53:46 +0300

    Task completed.

--- src/main/java/com/epam/mjc/io/TheNewReader.java
+++ src/main/java/com/epam/mjc/io/TheNewReader.java
@@ -1,16 +1,17 @@
 package com.epam.mjc.io;
 
 import java.io.*;
+import java.io.FileReader;
+
+public class TheNewReader {
 
 
-public class FileReader {
-
-    public Profile getDataFromFile(File file) {
+    public static void main(String[] args) {
 
         StringWriter myStringWriter = new StringWriter();
 
         try {
-            BufferedReader myBufferedReader = new BufferedReader(new java.io.FileReader(file));
+            BufferedReader myBufferedReader = new BufferedReader(new FileReader("src/main/resources/Profile.txt"));
             String theInfo;
             while ((theInfo = myBufferedReader.readLine()) != null) {
                 myStringWriter.write(theInfo);
@@ -27,17 +28,15 @@ public class FileReader {
 
         String theAge = theDataFromMyStringWriter.substring(theDataFromMyStringWriter.indexOf("Age:") + 5, theDataFromMyStringWriter.indexOf("Email:"));
         System.out.println(theAge);
-        int theAgeAsAnInteger = Integer.parseInt(theAge);
-
 
         String theEmail = theDataFromMyStringWriter.substring(theDataFromMyStringWriter.indexOf("Email:") + 7, theDataFromMyStringWriter.indexOf("Phone:"));
         System.out.println(theEmail);
 
         String thePhoneNumber = theDataFromMyStringWriter.substring(theDataFromMyStringWriter.indexOf("Phone:") + 7, theDataFromMyStringWriter.length());
         System.out.println(thePhoneNumber);
-        long phoneNumberAsLong = Long.parseLong(thePhoneNumber);
 
-        return new Profile(theName,theAgeAsAnInteger,theEmail,phoneNumberAsLong);
+
+
     }
 
 }
